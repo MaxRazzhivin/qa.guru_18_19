9f9ff3fcc0bdda490c56796370f029a13dd3ad62
Max_Razzhivin <max.nvo06@gmail.com>
2025-05-09 13:47:23 +0300

    trying fix problem with jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,21 +52,19 @@ def android_management():
 
 @pytest.fixture(scope='function')
 def browser_management():
-    browser.config.base_url=os.getenv(
-        'base_url', 'https://www.wikipedia.org'
-    )
-    browser.config.driver_name = os.getenv('driver_name', 'chrome')
+    browser.config.base_url= 'https://www.wikipedia.org'
+
+    browser.config.driver_name = 'chrome'
 
     # chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument('--headless')
     # browser.config.driver_options = chrome_options
 
-    browser.config.hold_driver_at_exit = (
-        os.getenv('hold_driver_at_exit', 'false').lower() == 'true'
-    )
-    browser.config.window_width = os.getenv('window_width', '1024')
-    browser.config.window_height = os.getenv('window_height', '768')
-    browser.config.timeout = float(os.getenv('timeout', '3.0'))
+    browser.config.hold_driver_at_exit = 'true'
+
+    browser.config.window_width = '1024'
+    browser.config.window_height = '768'
+    browser.config.timeout = float('3.0')
 
     yield
 
