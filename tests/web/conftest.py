04f598f3f9e2ac49e0c0dc674052a59b76363a82
Max_Razzhivin <max.nvo06@gmail.com>
2025-05-19 18:59:08 +0300

    add os.getenv in conftest

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,25 +1,26 @@
-import allure
+import os
+
 import pytest
 from allure_commons._allure import StepContext
 from selene import browser, support
-import os
-from appium import webdriver
-from dotenv import load_dotenv
-import config
 from utils import attach
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.base_url = 'https://www.wikipedia.org'
-    browser.config.driver_name = 'chrome'
+
+    browser.config.base_url = os.getenv('base_url', 'https://www.wikipedia.org')
+    browser.config.driver_name = os.getenv('driver_name', 'chrome')
+    browser.config._wait_decorator = support._logging.wait_with(context=StepContext)
 
     browser.config.hold_driver_at_exit = 'true'
 
-    browser.config.window_width = '1024'
-    browser.config.window_height = '768'
-    browser.config.timeout = float('3.0')
+    browser.config.window_width = os. getenv('window_width', '1900')
+    browser.config.window_height = os. getenv('window_height', '1200')
+    browser.config.timeout = float(os.getenv("timeout", '3.0'))
 
     yield
+
 
     attach.add_screenshot(browser)
     browser.quit()
